78c2463a83b4903909128540cb7edc80bafacb1e
Patricia Vila Luz <patriciavilaluz@hotmail.com>
2019-05-15 17:07:50 +0100

    YELP MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create(name: "Tambarina", address: "Rua Poco dos Negros", phone_number: "22242", category:"italian")
+Restaurant.create(name: "Luigis", address: "Rua Poco dos Negros", phone_number: "22242", category:"italian")
+Restaurant.create(name: "Blabla", address: "Rua Poco dos Negros", phone_number: "22242", category:"italian")
+# Restaurant.create(name: "Luigis", address: "Santos")
+# Restaurant.create(name: "Quick Asia", address: "Santos")
+# Restaurant.create(name: "Patricia~s", address: "Santos")
+# Restaurant.create(name: "Paramo-Bebemo", address: "Santos")
